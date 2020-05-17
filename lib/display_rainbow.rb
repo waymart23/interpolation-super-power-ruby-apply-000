8e4c6e95578b68682e660c75184b5d1e0308c734
Wayne Martin <waymart23@gmail.com>
2020-05-17 01:55:09 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 def display_rainbow(colors)=["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 puts {R:red[0]}
 puts {O:orange[1]}
-puts {Y:yellow [2]}
+puts {Y:yellow[2]}
 puts {G:green[3]}
 puts {B:blue[4]}
 puts {I:indigo[5]}
